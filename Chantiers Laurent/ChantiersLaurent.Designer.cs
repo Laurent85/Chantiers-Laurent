20ce0873e8423bed297f091867594159aeb90379
Laurent85 <manceaulaurent@yahoo.fr>
2019-04-19 21:30:36 +0200

    21:30

--- Chantiers Laurent/ChantiersLaurent.Designer.cs
+++ Chantiers Laurent/ChantiersLaurent.Designer.cs
@@ -33,11 +33,24 @@
             this.btnTraitement = new System.Windows.Forms.Button();
             this.btnFichierExcelDépart = new System.Windows.Forms.Button();
             this.lblCheminFichierModèle = new System.Windows.Forms.Label();
+            this.bgwExcelFinal = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProgression = new System.Windows.Forms.Label();
+            this.txbRecherche = new System.Windows.Forms.TextBox();
+            this.btnEnregistrerFichier = new System.Windows.Forms.Button();
+            this.cbxRaisonSociale = new System.Windows.Forms.ComboBox();
+            this.bgwExcelBase = new System.ComponentModel.BackgroundWorker();
+            this.lblAttente = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -47,16 +60,16 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(31, 117);
+            this.dataGridView1.Location = new System.Drawing.Point(31, 185);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1310, 466);
+            this.dataGridView1.Size = new System.Drawing.Size(1310, 450);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnTraitement
             // 
-            this.btnTraitement.Location = new System.Drawing.Point(898, 13);
+            this.btnTraitement.Location = new System.Drawing.Point(31, 136);
             this.btnTraitement.Name = "btnTraitement";
-            this.btnTraitement.Size = new System.Drawing.Size(75, 23);
+            this.btnTraitement.Size = new System.Drawing.Size(87, 23);
             this.btnTraitement.TabIndex = 2;
             this.btnTraitement.Text = "Traitement";
             this.btnTraitement.UseVisualStyleBackColor = true;
@@ -64,7 +77,7 @@
             // 
             // btnFichierExcelDépart
             // 
-            this.btnFichierExcelDépart.Location = new System.Drawing.Point(31, 13);
+            this.btnFichierExcelDépart.Location = new System.Drawing.Point(31, 107);
             this.btnFichierExcelDépart.Name = "btnFichierExcelDépart";
             this.btnFichierExcelDépart.Size = new System.Drawing.Size(87, 23);
             this.btnFichierExcelDépart.TabIndex = 3;
@@ -75,23 +88,109 @@
             // lblCheminFichierModèle
             // 
             this.lblCheminFichierModèle.AutoSize = true;
-            this.lblCheminFichierModèle.Location = new System.Drawing.Point(124, 18);
+            this.lblCheminFichierModèle.ForeColor = System.Drawing.Color.Green;
+            this.lblCheminFichierModèle.Location = new System.Drawing.Point(124, 112);
             this.lblCheminFichierModèle.Name = "lblCheminFichierModèle";
-            this.lblCheminFichierModèle.Size = new System.Drawing.Size(35, 13);
+            this.lblCheminFichierModèle.Size = new System.Drawing.Size(0, 13);
             this.lblCheminFichierModèle.TabIndex = 4;
-            this.lblCheminFichierModèle.Text = "label1";
+            // 
+            // bgwExcelFinal
+            // 
+            this.bgwExcelFinal.WorkerReportsProgress = true;
+            this.bgwExcelFinal.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgwTraitementExcelFinal);
+            this.bgwExcelFinal.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BgwProgressionExcelFinal);
+            this.bgwExcelFinal.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgwTerminéExcelFinal);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(156, 136);
+            this.progressBar1.Maximum = 500;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(176, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // lblProgression
+            // 
+            this.lblProgression.AutoSize = true;
+            this.lblProgression.Location = new System.Drawing.Point(352, 141);
+            this.lblProgression.Name = "lblProgression";
+            this.lblProgression.Size = new System.Drawing.Size(0, 13);
+            this.lblProgression.TabIndex = 6;
+            // 
+            // txbRecherche
+            // 
+            this.txbRecherche.BackColor = System.Drawing.SystemColors.Window;
+            this.txbRecherche.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbRecherche.ForeColor = System.Drawing.Color.Black;
+            this.txbRecherche.Location = new System.Drawing.Point(623, 138);
+            this.txbRecherche.Name = "txbRecherche";
+            this.txbRecherche.Size = new System.Drawing.Size(359, 20);
+            this.txbRecherche.TabIndex = 7;
+            this.txbRecherche.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txbRecherche.TextChanged += new System.EventHandler(this.txbRecherche_TextChanged);
+            // 
+            // btnEnregistrerFichier
+            // 
+            this.btnEnregistrerFichier.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEnregistrerFichier.Location = new System.Drawing.Point(1193, 136);
+            this.btnEnregistrerFichier.Name = "btnEnregistrerFichier";
+            this.btnEnregistrerFichier.Size = new System.Drawing.Size(148, 23);
+            this.btnEnregistrerFichier.TabIndex = 8;
+            this.btnEnregistrerFichier.Text = "Enregistrer le fichier excel...";
+            this.btnEnregistrerFichier.UseVisualStyleBackColor = true;
+            this.btnEnregistrerFichier.Click += new System.EventHandler(this.btnEnregistrerFichier_Click);
+            // 
+            // cbxRaisonSociale
+            // 
+            this.cbxRaisonSociale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.cbxRaisonSociale.FormattingEnabled = true;
+            this.cbxRaisonSociale.Location = new System.Drawing.Point(623, 104);
+            this.cbxRaisonSociale.Name = "cbxRaisonSociale";
+            this.cbxRaisonSociale.Size = new System.Drawing.Size(359, 21);
+            this.cbxRaisonSociale.Sorted = true;
+            this.cbxRaisonSociale.TabIndex = 9;
+            this.cbxRaisonSociale.SelectedIndexChanged += new System.EventHandler(this.cbxRaisonSociale_SelectedIndexChanged);
+            // 
+            // bgwExcelBase
+            // 
+            this.bgwExcelBase.WorkerReportsProgress = true;
+            this.bgwExcelBase.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgwTraitementExcelBase);
+            this.bgwExcelBase.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BgwProgressionExcelBase);
+            this.bgwExcelBase.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgwTerminéExcelBase);
+            // 
+            // lblAttente
+            // 
+            this.lblAttente.AutoSize = true;
+            this.lblAttente.BackColor = System.Drawing.Color.Transparent;
+            this.lblAttente.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAttente.ForeColor = System.Drawing.Color.Red;
+            this.lblAttente.Location = new System.Drawing.Point(317, 350);
+            this.lblAttente.Name = "lblAttente";
+            this.lblAttente.Size = new System.Drawing.Size(665, 37);
+            this.lblAttente.TabIndex = 10;
+            this.lblAttente.Text = "Chargement du fichier. Veuillez patienter...";
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1370, 601);
+            this.ClientSize = new System.Drawing.Size(1370, 653);
+            this.Controls.Add(this.lblAttente);
+            this.Controls.Add(this.cbxRaisonSociale);
+            this.Controls.Add(this.btnEnregistrerFichier);
+            this.Controls.Add(this.txbRecherche);
+            this.Controls.Add(this.lblProgression);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.lblCheminFichierModèle);
             this.Controls.Add(this.btnFichierExcelDépart);
             this.Controls.Add(this.btnTraitement);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Principal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -104,6 +203,14 @@
         private System.Windows.Forms.Button btnTraitement;
         private System.Windows.Forms.Button btnFichierExcelDépart;
         private System.Windows.Forms.Label lblCheminFichierModèle;
+        private System.ComponentModel.BackgroundWorker bgwExcelFinal;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProgression;
+        private System.Windows.Forms.TextBox txbRecherche;
+        private System.Windows.Forms.Button btnEnregistrerFichier;
+        private System.Windows.Forms.ComboBox cbxRaisonSociale;
+        private System.ComponentModel.BackgroundWorker bgwExcelBase;
+        private System.Windows.Forms.Label lblAttente;
     }
 }
 
--- Chantiers Laurent/ChantiersLaurent.cs
+++ Chantiers Laurent/ChantiersLaurent.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Office.Interop.Excel;
 using System;
+using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
 using System.IO;
@@ -10,9 +11,25 @@ namespace Chantiers_Laurent
 {
     public partial class Principal : Form
     {
+        public int Compteur;
+        public int Progression;
+        public string CheminModèle;
+        private readonly System.Data.DataTable _data = new System.Data.DataTable();
+
         public Principal()
         {
             InitializeComponent();
+        }
+
+        [STAThread]
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            btnTraitement.Enabled = false;
+            cbxRaisonSociale.Enabled = false;
+            txbRecherche.Enabled = false;
+            btnEnregistrerFichier.Enabled = false;
+            dataGridView1.DoubleBuffered(true);
+            lblAttente.Visible = false;
         }
 
         private void BtnFicherExcelDépart(object sender, EventArgs e)
@@ -37,51 +54,80 @@ namespace Chantiers_Laurent
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 lblCheminFichierModèle.Text = openFileDialog1.FileName;
-
-                var strPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Modèle.xlsx";
-                if (File.Exists(strPath)) File.Delete(strPath);
-                var assembly = Assembly.GetExecutingAssembly();
-                var input = assembly.GetManifestResourceStream("Chantiers_Laurent.Resources.Modèle.xlsx");
-                var output = File.Open(strPath, FileMode.CreateNew);
-                CopieFichierModèle(input, output);
-                input?.Dispose();
-                output.Dispose();
-
-                Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
-                xlApp.Visible = false;
-                xlApp.DisplayAlerts = false;
-                Workbook fichierExcelDépart = xlApp.Workbooks.Open(lblCheminFichierModèle.Text);
-                Worksheet feuilleDépart = fichierExcelDépart.Worksheets[1];
-                Range colonnes12 = feuilleDépart.Range[feuilleDépart.Cells[1, 1], feuilleDépart.Cells[1, 2]];
-                colonnes12.UnMerge();
-
-                feuilleDépart.Range["B1"].EntireColumn.Delete();
-                if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx")) File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx");
-                fichierExcelDépart.SaveAs(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx");
-
-                String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
-                                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx" +
-                                ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
-
-                OleDbConnection con = new OleDbConnection(constr);
-                con.Open();
-
-                //OleDbCommand oconn = new OleDbCommand("Select * From [" + feuilleDépart.Name + "$] WHERE [Nom de l'offre / du chantier] LIKE '%ICF%'", con);
-                OleDbCommand oconn = new OleDbCommand("Select * From [" + feuilleDépart.Name + "$]", con);
-
-                OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
-                System.Data.DataTable data = new System.Data.DataTable();
-                sda.Fill(data);
-                dataGridView1.DataSource = data;
-
-                con.Close();
-                fichierExcelDépart.Close();
-                xlApp.Quit();
-                GC.Collect();
             }
+
+            bgwExcelBase.RunWorkerAsync();
+        }
+
+        private void BgwTraitementExcelBase(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            lblAttente.Invoke(new MethodInvoker(delegate
+            {
+                lblAttente.Parent = dataGridView1;
+    lblAttente.Visible = true;
+}));
+            var strPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Modèle.xlsx";
+            if (File.Exists(strPath)) File.Delete(strPath);
+            var assembly = Assembly.GetExecutingAssembly();
+            var input = assembly.GetManifestResourceStream("Chantiers_Laurent.Resources.Modèle.xlsx");
+            var output = File.Open(strPath, FileMode.CreateNew);
+            CopieFichierModèle(input, output);
+            input?.Dispose();
+            output.Dispose();
+
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            xlApp.Visible = false;
+            xlApp.DisplayAlerts = false;
+            Workbook fichierExcelDépart = xlApp.Workbooks.Open(lblCheminFichierModèle.Text);
+            Worksheet feuilleDépart = fichierExcelDépart.Worksheets[1];
+            Range colonnes12 = feuilleDépart.Range[feuilleDépart.Cells[1, 1], feuilleDépart.Cells[1, 2]];
+            colonnes12.UnMerge();
+
+            feuilleDépart.Range["B1"].EntireColumn.Delete();
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx")) File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx");
+            fichierExcelDépart.SaveAs(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx");
+
+            String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx" +
+                            ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
+
+            OleDbConnection con = new OleDbConnection(constr);
+            con.Open();
+
+            //OleDbCommand oconn = new OleDbCommand("Select * From [" + feuilleDépart.Name + "$] WHERE [Nom de l'offre / du chantier] LIKE '%ICF%'", con);
+            OleDbCommand oconn = new OleDbCommand("Select * From [" + feuilleDépart.Name + "$]", con);
+
+            OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            System.Data.DataTable data = new System.Data.DataTable();
+            sda.Fill(data);
+
+            dataGridView1.Invoke(new MethodInvoker(delegate
+{
+    dataGridView1.DataSource = data;
+    if (dataGridView1.Rows.Count > 0) btnTraitement.Enabled = true;
+}));
+
+            con.Close();
+            fichierExcelDépart.Close(0);
+            xlApp.Quit();
+            GC.Collect();
+        }
+
+        private void BgwProgressionExcelBase(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+        }
+
+        private void BgwTerminéExcelBase(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            lblAttente.Visible = false;
         }
 
         private void BtnTraitementFichierExcel(object sender, EventArgs e)
+        {
+            bgwExcelFinal.RunWorkerAsync();
+        }
+
+        private void BgwTraitementExcelFinal(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
             Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
             xlApp.Visible = false;
@@ -89,7 +135,7 @@ namespace Chantiers_Laurent
 
             Workbook fichierExcelDépart = xlApp.Workbooks.Open(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Base.xlsx");
             Worksheet feuilleDépart = fichierExcelDépart.Worksheets[1];
-            
+
             Workbook fichierExcelModèle = xlApp.Workbooks.Open(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Modèle.xlsx");
             Worksheet feuilleModèle = fichierExcelModèle.Worksheets[1];
 
@@ -98,6 +144,7 @@ namespace Chantiers_Laurent
                                Missing.Value, Missing.Value,
                                XlSearchOrder.xlByRows, XlSearchDirection.xlPrevious,
                                false, Missing.Value, Missing.Value).Row;
+            Compteur = dernierRang;
 
             for (int i = 2; i <= dernierRang; i++)
             {
@@ -174,17 +221,19 @@ namespace Chantiers_Laurent
                         {
                             Range ligneDestination1 = feuilleModèle.Range[feuilleModèle.Cells[ligneModèle, 1],
                                 feuilleModèle.Cells[ligneModèle, 27]];
-                            ligneDestination1.Interior.Color = Microsoft.Office.Interop.Excel.XlRgbColor.rgbLightGrey;
+                            ligneDestination1.Interior.Color = XlRgbColor.rgbLightGrey;
                         }
                         else
                         {
                             Range ligneDestination1 = feuilleModèle.Range[feuilleModèle.Cells[ligneModèle, 1],
                                 feuilleModèle.Cells[ligneModèle, 27]];
-                            ligneDestination1.Interior.Color = Microsoft.Office.Interop.Excel.XlRgbColor.rgbWhite;  
+                            ligneDestination1.Interior.Color = XlRgbColor.rgbWhite;
                         }
                         ligneModèle++;
                     }
                 }
+                bgwExcelFinal.ReportProgress(i);
+                Progression = i;
             }
             fichierExcelModèle.Save();
 
@@ -198,18 +247,39 @@ namespace Chantiers_Laurent
             //OleDbCommand oconn = new OleDbCommand("Select * From [" + feuilleDépart.Name + "$] WHERE [Nom de l'offre / du chantier] LIKE '%ICF%'", con);
             OleDbCommand oconn = new OleDbCommand("Select * From [" + feuilleModèle.Name + "$A5:AA5000]", con);
             OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
-            System.Data.DataTable data = new System.Data.DataTable();
-            sda.Fill(data);
-            dataGridView1.DataSource = data;
-            dataGridView1.RowsDefaultCellStyle.BackColor = Color.Bisque;
-            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor =
-                Color.Beige;
-            con.Close();
+            //System.Data.DataTable data = new System.Data.DataTable();
+            sda.Fill(_data);
+            dataGridView1.Invoke(new MethodInvoker(delegate
+{
+    dataGridView1.DataSource = _data;
+    dataGridView1.RowsDefaultCellStyle.BackColor = Color.Bisque;
+    dataGridView1.AlternatingRowsDefaultCellStyle.BackColor =
+        Color.Beige;
+    con.Close();
+}));
 
-            fichierExcelDépart.Close();
-            fichierExcelModèle.Close();
+            fichierExcelDépart.Close(0);
+            fichierExcelModèle.Close(0);
             xlApp.Quit();
             GC.Collect();
+            RemplirCombobox();
+        }
+
+        private void BgwProgressionExcelFinal(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Maximum = Compteur;
+            progressBar1.Value = e.ProgressPercentage;
+            lblProgression.Text = Progression + @" / " + Compteur;
+        }
+
+        private void BgwTerminéExcelFinal(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 0;
+            lblProgression.Text = @"Traitement terminé !";
+            cbxRaisonSociale.Enabled = true;
+            txbRecherche.Enabled = true;
+            btnEnregistrerFichier.Enabled = true;
+            cbxRaisonSociale.SelectedIndex = 0;
         }
 
         private void CopieFichierModèle(Stream input, Stream output)
@@ -222,6 +292,125 @@ namespace Chantiers_Laurent
                     return;
                 output.Write(buffer, 0, read);
             }
+        }
+
+        private void txbRecherche_TextChanged(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(_data)
+            {
+                RowFilter = "[Raison sociale] like '" + txbRecherche.Text + "%' OR [Raison sociale1] like '" +
+                            txbRecherche.Text + "%' OR [Raison sociale2] like '" + txbRecherche.Text +
+                            "%' OR [Raison sociale3] like '" + txbRecherche.Text + "%'"
+            };
+            dataGridView1.DataSource = dv;
+
+            if (dataGridView1.Rows.Count == 1)
+            {
+                foreach (DataGridViewColumn colonne in dataGridView1.Columns)
+                {
+                    if (dataGridView1.Rows[0].Cells[colonne.Name].Value.ToString() == "")
+                    {
+                        colonne.Visible = false;
+                    }
+                    else
+                    {
+                        colonne.Visible = true;
+                    }
+                }
+            }
+            else
+            {
+                foreach (DataGridViewColumn colonne in dataGridView1.Columns)
+                {
+                    colonne.Visible = true;
+                }
+            }
+
+            if (dataGridView1.Rows.Count > 0 && txbRecherche.Text != "")
+            {
+                txbRecherche.BackColor = Color.LightGreen;
+            }
+            if (dataGridView1.Rows.Count == 0 && txbRecherche.Text != "")
+            {
+                txbRecherche.BackColor = Color.Red;
+            }
+            if (txbRecherche.Text == "")
+            {
+                txbRecherche.BackColor = Color.White;
+            }
+        }
+
+        private void RemplirCombobox()
+        {
+            dataGridView1.Invoke(new MethodInvoker(delegate
+{
+    cbxRaisonSociale.Items.Clear();
+
+    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+    {
+        if (dataGridView1[11, i].Value != null)
+        {
+            if (!cbxRaisonSociale.Items.Contains(dataGridView1[11, i].Value.ToString()))
+                cbxRaisonSociale.Items.Add(dataGridView1[11, i].Value.ToString());
+        }
+        if (dataGridView1[15, i].Value != null)
+        {
+            if (!cbxRaisonSociale.Items.Contains(dataGridView1[15, i].Value.ToString()))
+                cbxRaisonSociale.Items.Add(dataGridView1[15, i].Value.ToString());
+        }
+        if (dataGridView1[19, i].Value != null)
+        {
+            if (!cbxRaisonSociale.Items.Contains(dataGridView1[19, i].Value.ToString()))
+                cbxRaisonSociale.Items.Add(dataGridView1[19, i].Value.ToString());
+        }
+        if (dataGridView1[23, i].Value != null)
+        {
+            if (!cbxRaisonSociale.Items.Contains(dataGridView1[23, i].Value.ToString()))
+                cbxRaisonSociale.Items.Add(dataGridView1[23, i].Value.ToString());
+        }
+    }
+
+    for (int i = 0; i < cbxRaisonSociale.Items.Count; i++)
+    {
+        if (cbxRaisonSociale.GetItemText(cbxRaisonSociale.Items[i]) == "")
+        {
+            cbxRaisonSociale.Items.Remove(cbxRaisonSociale.Items[i]);
+        }
+    }
+
+    cbxRaisonSociale.Items.Add("***AUCUN FILTRE***");
+}));
+        }
+
+        private void btnEnregistrerFichier_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var folderPath = folderBrowserDialog1.SelectedPath;
+                File.Copy(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Modèle.xlsx",
+                    folderPath + @"\Chantiers.xlsx");
+            }
+        }
+
+        private void cbxRaisonSociale_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txbRecherche.Text = cbxRaisonSociale.Text;
+            if (cbxRaisonSociale.Text == @"***AUCUN FILTRE***")
+            {
+                txbRecherche.Text = "";
+            }
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static void DoubleBuffered(this DataGridView dgv, bool setting)
+        {
+            Type dgvType = dgv.GetType();
+            PropertyInfo pi = dgvType.GetProperty("DoubleBuffered",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            if (pi != null) pi.SetValue(dgv, setting, null);
         }
     }
 }
